e33b2badc3ce120a89f6e2144223caaa3e0aecdb
Cebasthian <sebmoe-2@student.ltu.se>
2024-11-18 12:04:00 +0100

    init

---
+++ src/main/java/com/example/digitalpassport/DigitalPassportApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalpassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPassportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/digitalpassport/DigitalPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalpassport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalPassportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
