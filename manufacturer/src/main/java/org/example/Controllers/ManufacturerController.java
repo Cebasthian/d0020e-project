0bc74acb63fc279be78c1af0d7ac27911f1dfb61
Tomtentxt <114141867+Tomtentxt@users.noreply.github.com>
2025-01-26 14:39:01 +0100

    Added what I wanted to import.

--- manufacturer/src/main/java/org/example/Controllers/ManufacturerController.java
+++ manufacturer/src/main/java/org/example/Controllers/ManufacturerController.java
@@ -4,6 +4,7 @@ import org.example.Entity.PC;
 import org.example.Services.ManufacturerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
