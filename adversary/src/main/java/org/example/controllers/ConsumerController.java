54910660a811b9247b4c0ed7805d9976259ab9e0
Samuels Desktop <cool23man23@gmail.com>
2025-02-20 17:28:30 +0100

    Set up and tested edc gui on all 4 modules. Tested successfully using docker.

---
+++ adversary/src/main/java/org/example/controllers/ConsumerController.java
@@ -0,0 +1,8 @@
+package org.example.controllers;
+
+import com.example.edcinterface.controller.EdcConsumerController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController extends EdcConsumerController {
+}
